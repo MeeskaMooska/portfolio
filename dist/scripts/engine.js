2b82a52c15f1f2387ce0bf94cb144eb45c90bf1b
MeeskaMooska <77514959+MeeskaMooska@users.noreply.github.com>
2023-11-06 16:40:28 -0500

    restructuring

---
+++ dist/scripts/engine.js
@@ -0,0 +1,25 @@
+function scrollToSection(id) {
+    const element = document.getElementById(id);
+    element.scrollIntoView({ behavior: 'smooth' });
+  }
+  
+  function changeServiceValue(val) {
+    document.getElementById("services").value = val;
+  }
+  
+  function preventEnterSubmit(event) {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+    }
+  }
+  
+  function showSuccessMessage() {
+    var successMessage = document.getElementById("successMessage");
+    successMessage.classList.add("show");
+  
+    // Automatically hide the success message after 5 seconds
+    setTimeout(function() {
+      successMessage.classList.remove("show");
+    }, 5000);
+  }
+  
